e12984d91a6af85b341b80747e320faa7412311a
IsmailErdem-Er <IsmailErdem.Er@vodafone.com>
2024-11-07 19:07:38 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,65 @@
+import org.gradle.kotlin.dsl.android
+import org.gradle.kotlin.dsl.libs
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.kotlin.android.ksp)
+}
+
+android {
+    namespace = "com.example.githubusersearcher"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.githubusersearcher"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.moshi.converter)
+    implementation(libs.okhttp3)
+    implementation(libs.okhttp3.logging.interceptor)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/example/githubusersearcher/GithubUserSearcherActivity.kt
@@ -0,0 +1,10 @@
+package com.example.githubusersearcher
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GithubUserSearcherActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
